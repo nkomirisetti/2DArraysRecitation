ac79233bfcd397e85cca6fab569d9c6552fde85d
Nikhil Komirisetti <nikhilk@live.unc.edu>
2019-09-05 23:03:19 -0400

    initial commit

---
+++ src/app/Transpose.java
@@ -0,0 +1,26 @@
+package app;
+
+public class Transpose {
+    public static void main(String[] args) {
+        testMethod();
+    }
+
+    public static int[][] transposeArray(int[][] inputArray){
+        // put your code here!
+    }
+
+    public static void testMethod(){
+        int[][] test1 = {{1,2,3},
+                         {4,5,6},
+                         {7,8,9}};
+        int[][] test1Output = {{1,4,7},
+                               {2,5,8},
+                               {3,6,9}};
+
+        if (transposeArray(test1).equals(test1Output)){
+            System.out.println("Yay!");
+        } else{
+            System.out.println("try again!");
+        }
+    }
+}
